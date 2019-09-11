7364c79fc9e3cd01c4536599261d00da54580391
Elena Danish <elenadanis2032@gmail.com>
2019-09-11 15:31:04 +0300

    my first ajx with json-server

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,59 +1,29 @@
 import React from 'react';
 import styles from './users.module.css';
+import axios from "axios";
+
+debugger;
 
 let Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers([
-                {
-                    id: 1,
-                    photoUrl: 'https://house-animals.ru/sites/default/files/media/user-1/trehcvetnaya-koshka-256.jpg',
-                    followed: false,
-                    fullName: 'Tomas',
-                    status: 'I like fish',
-                    location: {city: 'Petropavl', country: 'Kazakhstan'}
-                },
-                {
-                    id: 2,
-                    photoUrl: 'https://ranbus.fra1.cdn.digitaloceanspaces.com/images/avatars/2019/01/154745223793wK7X5xCi.jpg',
-                    followed: true,
-                    fullName: 'Silvester',
-                    status: 'I live in Moscow!',
-                    location: {city: 'Moscow', country: 'Russia'}
-                },
-                {
-                    id: 3,
-                    photoUrl: 'https://house-animals.ru/sites/default/files/media/user-1/grey-cat-256.jpg',
-                    followed: false,
-                    fullName: 'Keks',
-                    status: 'Hi, I very happy cat!',
-                    location: {city: 'Ekaterinburg', country: 'Russia'}
-                },
-                {
-                    id: 4,
-                    photoUrl: 'https://house-animals.ru/sites/default/files/media/user-1/redhead-cat-256.jpg',
-                    followed: false,
-                    fullName: 'Semen',
-                    status: 'Hello everyone!',
-                    location: {city: 'Perm', country: 'Russia'}
-                },
-                {
-                    id: 5,
-                    photoUrl: 'https://www.stylist.co.uk/images/app/uploads/2017/10/24184627/d82c29b23a574d0de11df28351edde1e-256x256.jpg?w=256&h=256&fit=max&auto=format%2Ccompress',
-                    followed: true,
-                    fullName: 'Salvador',
-                    status: 'Hello, let\'s go to cinema!',
-                    location: {city: 'Ekaterinburg', country: 'Russia'}
-                }
-            ]
-        )
+        axios.get(`http://localhost:3000/users`, {
+            headers: {
+                "Content-type": "application/json"
+            }
+        })
+            .then(response => {
+                props.setUsers(response.data)
+            })
     }
 
-    return <div>
+
+
+    return <div >
         {
-            props.users.map(u => <div key={u.id}>
+            props.users.map(u => <div key={u.id} className={styles.user}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={styles.userPhoto}/>
+                         <img src={u.photoUrl} className={styles.userPhoto}/>
                     </div>
                     <div>
                         {u.followed
@@ -68,17 +38,18 @@ let Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </span>
                 </span>
             </div>)
         }
     </div>
 }
+        export default Users;
 
-export default Users;
+
